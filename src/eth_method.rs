5d4439c62f43193a2edbc3f57e567c041911ca24
Michael Birch <michael.birch@aurora.dev>
2022-03-02 03:23:56 +0100

    Define eth_sendRawTransaction RPC method

--- src/eth_method.rs
+++ src/eth_method.rs
@@ -1,11 +1,15 @@
+use aurora_engine_transactions::EthTransactionKind;
+
 pub enum EthMethod {
     GetTransactionReceipt([u8; 32]),
+    SendRawTransaction(Box<EthTransactionKind>),
 }
 
 impl EthMethod {
-    pub fn name(&self) -> &str {
+    pub fn name(&self) -> &'static str {
         match &self {
             Self::GetTransactionReceipt(_) => "eth_getTransactionReceipt",
+            Self::SendRawTransaction(_) => "eth_sendRawTransaction",
         }
     }
 
@@ -13,6 +17,10 @@ impl EthMethod {
         match &self {
             Self::GetTransactionReceipt(tx_hash) => {
                 vec![format!("0x{}", hex::encode(tx_hash))]
+            }
+            Self::SendRawTransaction(tx) => {
+                let tx_bytes: Vec<u8> = tx.as_ref().into();
+                vec![format!("0x{}", hex::encode(tx_bytes))]
             }
         }
     }
