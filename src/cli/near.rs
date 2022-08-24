e295a740fdc93ee6d40b1fd3d8961f4324679ead
Michael Birch <michael.birch@aurora.dev>
2022-08-24 17:58:10 +0200

    Add ability to dry-run xcc

--- src/cli/near.rs
+++ src/cli/near.rs
@@ -34,6 +34,22 @@ pub enum ReadCommand {
         #[clap(short, long)]
         input_data_hex: String,
     },
+    EngineXccDryRun {
+        #[clap(short, long)]
+        sender_address_hex: String,
+        #[clap(short, long)]
+        target_near_account: String,
+        #[clap(short, long)]
+        method_name: String,
+        #[clap(short, long)]
+        json_args: Option<String>,
+        #[clap(long)]
+        json_args_stdin: Option<bool>,
+        #[clap(short, long)]
+        deposit_yocto: Option<String>,
+        #[clap(short, long)]
+        attached_gas: Option<String>,
+    },
     GetBridgedNep141 {
         erc_20_address_hex: String,
     },
@@ -100,6 +116,35 @@ pub async fn execute_command<T: AsRef<str>>(
                     .unwrap();
                 println!("{:?}", result);
             }
+            ReadCommand::EngineXccDryRun {
+                target_near_account,
+                sender_address_hex,
+                method_name,
+                json_args,
+                json_args_stdin,
+                deposit_yocto,
+                attached_gas,
+            } => {
+                let promise = PromiseArgs::Create(parse_xcc_args(
+                    target_near_account,
+                    method_name,
+                    json_args,
+                    json_args_stdin,
+                    deposit_yocto,
+                    attached_gas,
+                ));
+                let precompile_args = CrossContractCallArgs::Eager(promise);
+                let sender = Address::decode(&sender_address_hex).unwrap();
+                let result = client
+                    .view_contract_call(
+                        sender,
+                        aurora_engine_precompiles::xcc::cross_contract_call::ADDRESS,
+                        Wei::zero(),
+                        precompile_args.try_to_vec().unwrap(),
+                    )
+                    .await?;
+                println!("{:?}", result);
+            }
             ReadCommand::GetBridgedNep141 { erc_20_address_hex } => {
                 let erc20 = Address::decode(&erc_20_address_hex).unwrap();
                 match client.get_nep141_from_erc20(erc20).await {
@@ -133,50 +178,14 @@ pub async fn execute_command<T: AsRef<str>>(
                 let source_private_key_hex = config.get_evm_secret_key();
                 let sk_bytes = utils::hex_to_arr32(source_private_key_hex)?;
                 let sk = secp256k1::SecretKey::parse(&sk_bytes).unwrap();
-                let near_args = match json_args {
-                    Some(args) => args.into_bytes(),
-                    None => match json_args_stdin {
-                        Some(true) => {
-                            let mut buf = String::new();
-                            std::io::Read::read_to_string(&mut std::io::stdin(), &mut buf).unwrap();
-                            buf.into_bytes()
-                        }
-                        None | Some(false) => Vec::new(),
-                    },
-                };
-                let attached_balance = match deposit_yocto {
-                    Some(x) => Yocto::new(x.parse().unwrap()),
-                    None => Yocto::new(0),
-                };
-                // TODO: there is an issue with the NEAR nonce tracking if I do two calls in a row
-                /*if attached_balance.as_u128() > 0 {
-                    // If we want to spend NEAR then we need to approve the precompile to spend our wNEAR.
-                    const APPROVE_SELECTOR: &[u8] = &[0x09u8, 0x5e, 0xa7, 0xb3];
-                    let input = [APPROVE_SELECTOR, &ethabi::encode(&[
-                        ethabi::Token::Address(aurora_engine_precompiles::xcc::cross_contract_call::ADDRESS.raw()),
-                        ethabi::Token::Uint(U256::from(u128::MAX)),
-                    ])].concat();
-                    let result = send_as_near_transaction(
-                        &client,
-                        &sk,
-                        Address::decode("34aadb3d3f359c7bfefa87f7a0ed4dbe5ba17d78").ok(),
-                        Wei::zero(),
-                        input,
-                    )
-                    .await?;
-                    println!("APPROVE: {:?}\n\n", result);
-                }*/
-                let attached_gas = match attached_gas {
-                    Some(gas) => NearGas::new(gas.parse().unwrap()),
-                    None => NearGas::new(30_000_000_000_000),
-                };
-                let promise = PromiseArgs::Create(PromiseCreateArgs {
-                    target_account_id: target_near_account.parse().unwrap(),
-                    method: method_name,
-                    args: near_args,
-                    attached_balance,
+                let promise = PromiseArgs::Create(parse_xcc_args(
+                    target_near_account,
+                    method_name,
+                    json_args,
+                    json_args_stdin,
+                    deposit_yocto,
                     attached_gas,
-                });
+                ));
                 let precompile_args = CrossContractCallArgs::Eager(promise);
                 let result = send_as_near_transaction(
                     client,
@@ -200,6 +209,42 @@ pub async fn execute_command<T: AsRef<str>>(
         },
     };
     Ok(())
+}
+
+fn parse_xcc_args(
+    target_near_account: String,
+    method_name: String,
+    json_args: Option<String>,
+    json_args_stdin: Option<bool>,
+    deposit_yocto: Option<String>,
+    attached_gas: Option<String>,
+) -> PromiseCreateArgs {
+    let near_args = match json_args {
+        Some(args) => args.into_bytes(),
+        None => match json_args_stdin {
+            Some(true) => {
+                let mut buf = String::new();
+                std::io::Read::read_to_string(&mut std::io::stdin(), &mut buf).unwrap();
+                buf.into_bytes()
+            }
+            None | Some(false) => Vec::new(),
+        },
+    };
+    let attached_balance = match deposit_yocto {
+        Some(x) => Yocto::new(x.parse().unwrap()),
+        None => Yocto::new(0),
+    };
+    let attached_gas = match attached_gas {
+        Some(gas) => NearGas::new(gas.parse().unwrap()),
+        None => NearGas::new(30_000_000_000_000),
+    };
+    PromiseCreateArgs {
+        target_account_id: target_near_account.parse().unwrap(),
+        method: method_name,
+        args: near_args,
+        attached_balance,
+        attached_gas,
+    }
 }
 
 async fn send_as_near_transaction<T: AsRef<str>>(
