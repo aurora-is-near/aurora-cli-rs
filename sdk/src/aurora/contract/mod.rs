d25f358cd0022f3867d84588f19cc13d6da6d7ce
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2025-06-05 10:59:18 +0100

    feat: introduce macro for view methods

---
+++ sdk/src/aurora/contract/mod.rs
@@ -0,0 +1,38 @@
+use aurora_engine_types::types::{Address, EthGas};
+use borsh::BorshDeserialize;
+use near_primitives::types::AccountId;
+
+use super::{ContractMethodResponse, error::Error};
+
+mod read;
+mod write;
+
+impl ContractMethodResponse for () {
+    fn parse(_rsp: Vec<u8>) -> Result<Self, Error> {
+        Ok(())
+    }
+}
+
+impl ContractMethodResponse for AccountId {
+    fn parse(rsp: Vec<u8>) -> Result<Self, Error> {
+        Self::try_from_slice(&rsp).map_err(Into::into)
+    }
+}
+
+impl ContractMethodResponse for Address {
+    fn parse(rsp: Vec<u8>) -> Result<Self, Error> {
+        Self::try_from_slice(&rsp).map_err(Into::into)
+    }
+}
+
+impl ContractMethodResponse for Option<EthGas> {
+    fn parse(rsp: Vec<u8>) -> Result<Self, Error> {
+        borsh::from_slice(&rsp).map_err(Into::into)
+    }
+}
+
+impl ContractMethodResponse for Option<Address> {
+    fn parse(rsp: Vec<u8>) -> Result<Self, Error> {
+        borsh::from_slice(&rsp).map_err(Into::into)
+    }
+}
